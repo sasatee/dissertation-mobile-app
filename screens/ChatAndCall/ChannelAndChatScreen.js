09d283c0a685c9a666b4697bec86b9db62319819
trainee <trainee@zapproach.com>
2024-06-14 13:11:03 +0400

    change in navigation

--- screens/ChatAndCall/ChannelAndChatScreen.js
+++ screens/ChatAndCall/ChannelAndChatScreen.js
@@ -41,15 +41,14 @@ export default function ChannelScreen({ route }) {
     //create a call using the channel members
     const call = VideoClient.call("default", Crypto.randomUUID());
     await call.getOrCreate({
-      ring:true,
+      ring: true,
       data: {
         members,
       },
     });
     //await call.join();
-    // console.log("CALLID",call.id)
 
-     navigation.navigate("Call", call.id);
+    navigation.navigate("Call", call.id);
   };
 
   return (
