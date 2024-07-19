e653e097bc1dd7a3baf934d9c11501d59ec6ced5
sasatee <sarvam2601@gmail.com>
2024-07-19 23:34:37 +0400

    Confirmation

--- validation/auth.js
+++ validation/auth.js
@@ -11,7 +11,7 @@ export const passwordValidationSchema = Yup.string()
   .required("Password is required")
   .min(8, "Password must be at least 8 characters long")
   .matches(/[a-z]/, "Password must contain at least one lowercase letter")
-  //.matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
 
   .matches(/[0-9]/, "Password must contain at least one number")
   .matches(
@@ -23,7 +23,7 @@ export const firstNameValidationSchema = Yup.string().required("This field  is r
 
 export const lastnameValidationSchema = Yup.string().required("This field  is required");
 
-// Define Yup validation schema for password confirmation
+//validation schema for password confirmation
 export const passwordConfirmationSchema = Yup.string()
   .required("Confirm password is required")
   .oneOf([Yup.ref("password"), null], "Passwords must match");
