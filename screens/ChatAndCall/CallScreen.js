aabcf5f9e3f8e1fe6e3388144366e40243ea014c
sasatee <sarvam2601@gmail.com>
2024-07-20 02:41:58 +0400

    permission granted for video call

--- screens/ChatAndCall/CallScreen.js
+++ screens/ChatAndCall/CallScreen.js
@@ -1,3 +1,63 @@
+// import React, { useEffect, useState } from "react";
+// import {
+//   CallContent,
+//   RingingCallContent,
+//   StreamCall,
+//   useCalls,
+//   useStreamVideoClient,
+// } from "@stream-io/video-react-native-sdk";
+
+// import { useNavigation } from "@react-navigation/native";
+
+// const CallScreen = ({ route }) => {
+//   const navigation = useNavigation();
+
+//   //const client = useStreamVideoClient();
+//   // const callId = route.params;
+
+//   // const [call, setCall] = useState(null);
+//   // useEffect(() => {
+//   //   const fetchCall = async () => {
+//   //     try {
+//   //       const call = client.call("default", callId);
+//   //       await call.get();
+//   //       setCall(call);
+//   //     } catch (err) {
+//   //       console.log(err.message);
+//   //     }
+//   //   };
+
+//   //   fetchCall();
+//   //   return () => {
+//   //     if (call) {
+//   //       call.leave();
+//   //     }
+//   //   };
+//   // }, [callId]);
+
+//   const calls = useCalls();
+//   const call = calls[0];
+
+//   if (!call) {
+//     if (navigation.canGoBack()) {
+//       navigation.goBack();
+//     } else {
+//       navigation.navigate("Chat",route?.params);
+//     }
+//     return null;
+//   }
+
+//   return (
+//     <StreamCall call={call}>
+//       {/* <CallContent /> */}
+//       <RingingCallContent />
+//     </StreamCall>
+//   );
+// };
+
+// export default CallScreen;
+
+
 import React, { useEffect, useState } from "react";
 import {
   CallContent,
@@ -11,46 +71,44 @@ import { useNavigation } from "@react-navigation/native";
 
 const CallScreen = ({ route }) => {
   const navigation = useNavigation();
-
-  //const client = useStreamVideoClient();
-  // const callId = route.params;
-
-  // const [call, setCall] = useState(null);
-  // useEffect(() => {
-  //   const fetchCall = async () => {
-  //     try {
-  //       const call = client.call("default", callId);
-  //       await call.get();
-  //       setCall(call);
-  //     } catch (err) {
-  //       console.log(err.message);
-  //     }
-  //   };
-
-  //   fetchCall();
-  //   return () => {
-  //     if (call) {
-  //       call.leave();
-  //     }
-  //   };
-  // }, [callId]);
+  const [isRinging, setIsRinging] = useState(false);
 
   const calls = useCalls();
   const call = calls[0];
 
-  if (!call) {
-    if (navigation.canGoBack()) {
-      navigation.goBack();
-    } else {
-      navigation.navigate("Chat",route?.params);
+  useEffect(() => {
+    if (!call) {
+      if (navigation.canGoBack()) {
+    
+        navigation.navigate("Chat", route?.params);
+      }
+      return;
     }
+
+    const handleCallStateChange = () => {
+      if (call.state.callingState === 'ringing') {
+        setIsRinging(true);
+      } else {
+        setIsRinging(false);
+      }
+    };
+
+    handleCallStateChange();
+
+    call.on('stateChanged', handleCallStateChange);
+
+    return () => {
+      call.off('stateChanged', handleCallStateChange);
+    };
+  }, [call, navigation, route]);
+
+  if (!call) {
     return null;
   }
 
   return (
     <StreamCall call={call}>
-      {/* <CallContent /> */}
-      <RingingCallContent />
+      {isRinging ? <RingingCallContent /> : <CallContent />}
     </StreamCall>
   );
 };
